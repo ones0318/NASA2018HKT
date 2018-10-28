fa6d07cd29319070c5cad7f20d511917a244dc3d
Yeh Shuyung <>
2018-10-29 02:13:08 +0800

    simplify

--- release_script.py
+++ release_script.py
@@ -12,13 +12,7 @@ def main():
     args = parser.parse_args()
     if os.path.isdir(args.release_dir):
         shutil.rmtree(args.release_dir)
-    os.mkdir(args.release_dir)
-    for cur_dir, dirs, files in os.walk(args.source_dir):
-        rel_path = os.path.relpath(cur_dir, args.source_dir)
-        for dir_ in dirs:
-            os.mkdir(os.path.join(args.release_dir, rel_path, dir_))
-        for file_ in files:
-            shutil.copy(os.path.join(cur_dir, file_), os.path.join(args.release_dir, rel_path, file_))
+    shutil.copytree(args.source_dir, args.release_dir)
     subprocess.check_call(['git', 'commit', '-m{0}'.format(args.release_version)])
     subprocess.check_call(['git', 'push'])
 
