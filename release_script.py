e3d3549ae3e62e3eb0de94a41e2f64adc32d0aa2
Yeh Shuyung <>
2018-10-31 21:30:24 +0800

    fix argument name

--- release_script.py
+++ release_script.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source-dir', help='Source folder')
     parser.add_argument('-r', '--release-dir', help='Release folder')
-    parser.add_argument('-v', '--releaes-version', help='Release version')
+    parser.add_argument('-v', '--release-version', help='Release version')
     args = parser.parse_args()
     if os.path.isdir(args.release_dir):
         shutil.rmtree(args.release_dir)
